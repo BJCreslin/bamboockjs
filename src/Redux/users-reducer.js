45b902ed6f0f137252eeee6d7fe489e3981e1ff6
bjcreslin <bjcreslin@gmail.com>
2020-03-11 17:27:14 +0700

    51

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -3,37 +3,7 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 
 const initialState = {
-    users: [
-        {
-            id: 1,
-            photoUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
-            followed: true,
-            fullName: "Ivanov",
-            status: "i am a boss",
-            location: {country: "usa", city: "london"}
-        }, {
-            id: 2,
-            photoUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
-            followed: false,
-            fullName: "petrov",
-            status: "google",
-            location: {country: "Kazakhstan", city: "Asatana"}
-        }, {
-            id: 3,
-            photoUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
-            followed: false,
-            fullName: "rtthn",
-            status: "google going",
-            location: {country: "Kirgyz", city: "fcsaer"}
-        }, {
-            id: 4,
-            photoUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
-            followed: true,
-            fullName: "Jonnson H",
-            status: "yandex roolitt",
-            location: {country: "Afgan", city: "Kabul"}
-        }
-    ]
+    users: []
 };
 
 let usersReducer = (state = initialState, action) => {
@@ -75,6 +45,6 @@ let usersReducer = (state = initialState, action) => {
 
 export const followActionCreator = (userId) => ({type: FOLLOW, userId: userId});
 export const unfollowActionCreator = (userId) => ({type: UNFOLLOW, userId: userId});
-export const setUsersActionCreator = (users) => ({type: SET_USERS, users:users});
+export const setUsersActionCreator = (users) => ({type: SET_USERS, users: users});
 
 export default usersReducer;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import css from './users.module.css';
+import * as axios from "axios";
+import photos from '../../assets/img/user.png';
 
 const Users = (props) => {
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        })
+        // props.users = [
+        //     {
+        //         id: 1,
+        //         photoUrl: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
+        //         followed: true,
+        //         fullName: "Ivanov",
+        //         status: "i am a boss",
+        //         location: {country: "usa", city: "london"}
+        //     }, {
+        //         id: 2,
+        //         photoUrl: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
+        //         followed: false,
+        //         fullName: "petrov",
+        //         status: "google",
+        //         location: {country: "Kazakhstan", city: "Asatana"}
+        //     }, {
+        //         id: 3,
+        //         photoUrl: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
+        //         followed: false,
+        //         fullName: "rtthn",
+        //         status: "google going",
+        //         location: {country: "Kirgyz", city: "fcsaer"}
+        //     }, {
+        //         id: 4,
+        //         photoUrl: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
+        //         followed: true,
+        //         fullName: "Jonnson H",
+        //         status: "yandex roolitt",
+        //         location: {country: "Afgan", city: "Kabul"}
+        //     }
+        // ];
+    }
     return <div className={css.mt}>
         {props.users.map(user =>
             <div key={user.id}>
             <span>
                 <div>
-                    <img className={css.userPhoto} src={user.photoUrl}/>
+                    <img className={css.userPhoto} src={user.photos.small != null ? user.photos.small : photos}/>
                 </div>
                 <div> {
                     user.followed ?
@@ -22,7 +60,7 @@ const Users = (props) => {
                     </span>
                 <span>
                     <div>
-                    {user.fullName}
+                    {user.name}
                     </div>
                     <div>
                     {user.status}
@@ -30,10 +68,10 @@ const Users = (props) => {
                     </span>
                 <span>
                     <div>
-                    {user.location.city}
+                    {"user.location.city"}
                     </div>
                     <div>
-                    {user.location.country}
+                    {"user.location.country"}
                     </div>
                     </span>
             </div>
