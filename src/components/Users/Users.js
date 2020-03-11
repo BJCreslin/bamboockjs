02369b6d4c238fb77d0428639d6c283ae42d84a1
bjcreslin <bjcreslin@gmail.com>
2020-03-11 22:32:22 +0700

    53

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,51 +1,22 @@
 import React from 'react';
-import css from './users.module.css';
 import * as axios from "axios";
-import photos from '../../assets/img/user.png';
+import css from "./users.module.css";
+import photos from "../../assets/img/user.png";
 
-const Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.setUsers(response.data.items);
-            })
-            // props.users = [
-            //     {
-            //         id: 1,
-            //         photoUrl: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
-            //         followed: true,
-            //         fullName: "Ivanov",
-            //         status: "i am a boss",
-            //         location: {country: "usa", city: "london"}
-            //     }, {
-            //         id: 2,
-            //         photoUrl: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
-            //         followed: false,
-            //         fullName: "petrov",
-            //         status: "google",
-            //         location: {country: "Kazakhstan", city: "Asatana"}
-            //     }, {
-            //         id: 3,
-            //         photoUrl: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
-            //         followed: false,
-            //         fullName: "rtthn",
-            //         status: "google going",
-            //         location: {country: "Kirgyz", city: "fcsaer"}
-            //     }, {
-            //         id: 4,
-            //         photoUrl: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-supermen-1574086730.png?crop=0.454xw:0.907xh;0.0176xw,0&resize=640:*",
-            //         followed: true,
-            //         fullName: "Jonnson H",
-            //         status: "yandex roolitt",
-            //         location: {country: "Afgan", city: "Kabul"}
-            //     }
-            // ];
-        }
-    };
-    return <div className={css.mt}>
-        <button onClick={() => (getUsers())}>Get Users</button>
-        {props.users.map(user =>
-            <div key={user.id}>
+
+class Users extends React.Component {
+
+    constructor(props) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items);
+        })
+    }
+
+    render() {
+        return <div className={css.mt}>
+            {this.props.users.map(user =>
+                    <div key={user.id}>
             <span>
                 <div>
                     <img className={css.userPhoto} src={user.photos.small != null ? user.photos.small : photos}/>
@@ -53,15 +24,15 @@ const Users = (props) => {
                 <div> {
                     user.followed ?
                         <button onClick={() => {
-                            props.unFollow(user.id)
+                            this.props.unFollow(user.id)
                         }}> Follow</button> :
                         <button onClick={() => {
-                            props.follow(user.id)
+                            this.props.follow(user.id)
                         }}> UnFollow</button>
                 }</div>
 
                     </span>
-                <span>
+                        <span>
                     <div>
                     {user.name}
                     </div>
@@ -69,7 +40,7 @@ const Users = (props) => {
                     {user.status}
                     </div>
                     </span>
-                <span>
+                        <span>
                     <div>
                     {"user.location.city"}
                     </div>
@@ -77,10 +48,12 @@ const Users = (props) => {
                     {"user.location.country"}
                     </div>
                     </span>
-            </div>
-        )
-        }
-    </div>
-};
+                    </div>
+            )
+            }
+        </div>
+    }
+
+}
 
 export default Users;
