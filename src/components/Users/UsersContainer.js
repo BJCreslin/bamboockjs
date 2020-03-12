bf95cdc9cc047ede86e57bc4cd4a54b43f71e800
bjcreslin <bjcreslin@gmail.com>
2020-03-12 21:56:49 +0700

    56 v2

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {connect} from "react-redux";
-import UsersApi from "./UsersApi";
 import {
     followActionCreator,
     setCurrentPageActionCreator,
@@ -8,6 +7,48 @@ import {
     setUsersActionCreator,
     unfollowActionCreator
 } from "../../Redux/users-reducer";
+import * as axios from "axios";
+import Users from "./Users";
+
+
+class UsersContainer extends React.Component {
+
+    componentDidMount = () => {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalCount(response.data.totalCount);
+        })
+    };
+
+    onCurrentPageChange = (newCurrentPage) => {
+        this.props.setCurrentPage(newCurrentPage);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page= ${newCurrentPage}&count=${this.props.numberForPage}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalCount(response.data.totalCount);
+        })
+
+
+    };
+
+    render() {
+        let pagesCount = Math.ceil(this.props.totalCount / this.props.numberForPage);
+        let pagesPagination = [];
+        let cycleBegin = this.props.currentPage - 10;
+        if (cycleBegin <= 0) cycleBegin = 1;
+        let cycleEnd = cycleBegin + 20;
+        if (cycleEnd > pagesCount) cycleEnd = pagesCount;
+        for (let i = cycleBegin; i < cycleEnd; i++) {
+            pagesPagination.push(i);
+        }
+
+
+        return <Users currentPage={this.props.currentPage} pagesPagination={pagesPagination} users={this.props.users}
+                      totalCount={this.props.totalCount} numberForPage={this.props.numberForPage}
+                      onCurrentPageChange={this.onCurrentPageChange} follow={this.props.follow}
+                      unFollow={this.props.unFollow}/>
+    }
+}
+
 
 const mapStateToProps = (state) => {
     return {
@@ -37,4 +78,4 @@ const mapDispatchToProps = (dispatch) => {
         }
     }
 };
-export default connect(mapStateToProps, mapDispatchToProps)(UsersApi);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
